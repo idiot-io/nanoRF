0ae32d1e3b6b81028e99a3429d21ef4916af8995
yair <yair99@gmail.com>
2017-05-17 16:58:21 +0300

    sending to reciever

--- examples/pulseSensor/PulseSensor.ino
+++ examples/pulseSensor/PulseSensor.ino
@@ -8,7 +8,23 @@
  
 #include "PulseSensorBPM.h"
 
+///////////// RF24 RADIO code ////////////
+#include <SPI.h>
+#include "RF24.h"
+byte addresses[][6] = {"1Node","2Node"};
+typedef enum { role_ping_out = 1, role_pong_back } role_e;                 // The various roles supported by this sketch
+role_e role ;
 
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);
+#define role_pin A3
+
+struct dataStruct{
+  unsigned long _micros;
+  int BPM;
+  int IBI;
+}myData;
+/////////////////////////////////////////
 
 /*
  * Pinout:
@@ -56,74 +72,99 @@ PulseSensorBPM pulseDetector(PIN_INPUT, MICROS_PER_READ / 1000L);
 volatile boolean QS;
 
 void setup() {
-  /*
-   * Use 115200 baud because that's what the Processing Sketch expects to read,
-   * and because that speed provides about 11 bytes per millisecond.
-   * 
-   * If we used a slower baud rate, we'd likely write bytes faster than
-   * they can be transmitted, which would mess up the timing
-   * of readSensor() calls, which would make the pulse measurement
-   * not work properly.
-   */
+  //Use 115200 baud 
   Serial.begin(115200);
 
-  // Set up the I/O pins
   
-  if (HAS_A_REF) {
-    analogReference(EXTERNAL);
-  }
-  // PIN_INPUT is set up by the pulseDetector constructor.
-  pinMode(PIN_BLINK, OUTPUT);
-  digitalWrite(PIN_BLINK, LOW);
-  pinMode(PIN_FADE, OUTPUT);
-  fadePWM = 0;
-  analogWrite(PIN_FADE, fadePWM);   // sets PWM duty cycle
+  radio.begin();
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  pinMode(role_pin, INPUT);
+  digitalWrite(role_pin, HIGH);
+  delay(20); // Just to get a solid reading on the role pin
+  // read the address pin, establish our role
+  
+  if ( digitalRead(role_pin) ) {
+    role = role_pong_back;
 
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1, addresses[0]);
+    radio.startListening();
+
+	// PIN_INPUT is set up by the pulseDetector constructor.
+	pinMode(PIN_BLINK, OUTPUT);
+	digitalWrite(PIN_BLINK, LOW);
+	pinMode(PIN_FADE, OUTPUT);
+	fadePWM = 0;
+	analogWrite(PIN_FADE, fadePWM);   // sets PWM duty cycle
+  
+    Serial.println("i receive data");
+  } else {
+    role = role_ping_out;
+
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1, addresses[1]);
+    radio.stopListening();
+
+    Serial.println("i send data");
+  }
+  
   QS = false;
   interruptSetup(); // start the interrupt timer.
 }
 
 void loop() {
   
-  /*
-   * Every so often, send the latest Sample to the Processing Sketch.
-   * We don't print every sample, because our baud rate
-   * won't support that much I/O.
-   */
-  delay(20);
-  
-  Serial.print('S');
-  Serial.println(pulseDetector.getSignal());
+	delay(20);
+	
+	if (role == role_ping_out)  
+	{ //sender
+		Serial.print('S');
+		Serial.println(pulseDetector.getSignal());
+		
+		// If the ISR has seen a beat, print the per-beat information.
+		if(QS){
+			myData.BPM = pulseDetector.getBPM();
+			myData.IBI = pulseDetector.getIBI();
+			bool ok = radio.write( &myData, sizeof(myData) );
+			QS = false;
+		}
+		
+	} 
+	else if ( role == role_pong_back )
+	{ //receiver
+		if ( radio.available()) {
+      // Variable for the received timestamp
+		while (radio.available()) {                          // While there is data ready
+			radio.read( &myData, sizeof(myData) );             // Get the payload
+		}
+			fadePWM = 255;  // start fading on the start of each beat.
+			analogWrite(PIN_FADE, fadePWM);
+			
+			Serial.print('B');
+			Serial.println(myData.BPM);
+			Serial.print('Q');
+			Serial.println(myData.IBI);
+			if (myData.BPM>80)
+			HIGHbpm();
+			if (myData.BPM<78)
+			LOWbpm();
+		}
+	
+		// Coincidentally, fade the LED a bit.
+		fadePWM -= PWM_STEPS_PER_FADE;
+		if (fadePWM < 0) {
+			fadePWM = 0;
+		}
+		analogWrite(PIN_FADE, fadePWM);
+		
+		// Blink the non-fading LED when the start of a pulse is detected.
+		if (pulseDetector.isPulse()) {
+			digitalWrite(PIN_BLINK, HIGH);
+		} else {
+			digitalWrite(PIN_BLINK, LOW);
+		}
+	}
 
-  // Coincidentally, fade the LED a bit.
-  fadePWM -= PWM_STEPS_PER_FADE;
-  if (fadePWM < 0) {
-    fadePWM = 0;
-  }
-  analogWrite(PIN_FADE, fadePWM);
-
-  // Blink the non-fading LED when the start of a pulse is detected.
-  if (pulseDetector.isPulse()) {
-    digitalWrite(PIN_BLINK, HIGH);
-  } else {
-    digitalWrite(PIN_BLINK, LOW);
-  }
-
-  // If the ISR has seen a beat, print the per-beat information.
-  if (QS) {
-    fadePWM = 255;  // start fading on the start of each beat.
-    analogWrite(PIN_FADE, fadePWM);
-    
-   Serial.print('B');
-    Serial.println(pulseDetector.getBPM());
-    Serial.print('Q');
-    Serial.println(pulseDetector.getIBI());
-    if (pulseDetector.getBPM()>80)
-    HIGHbpm();
-     if (pulseDetector.getBPM()<78)
-    LOWbpm();
-    QS = false;
-  }
 }
 
 void HIGHbpm(){
